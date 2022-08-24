148c16b1b6ecc6efc0528618696f0091522a106b
이 안토니의호 <ehlee3275@gmail.com>
2022-08-24 21:30:59 +0900

    [feature] education - 삭제 버튼 기능 구현 완료

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,72 +1,67 @@
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import EducationForm from "./EducationForm";
 import EducationCard from "./EducationCard";
 
-const Education = ({
-                       isEditable,
-                       setIsEditFormEditing,
-                       isEditFormEditing,
-                   }) => {
+const Education = ({ isEditable }) => {
+  const [isAdding, setIsAdding] = useState(false);
+  const toggleAddEducationForm = () => {
+    setIsAdding(!isAdding);
+  };
 
-    const [isAdding, setIsAdding] = useState(false);
-    const toggleAddEducationForm = () => {
-        setIsAdding(!isAdding);
-    };
+  const [educations, setEducations] = useState([]);
 
-    const [educations, setEducations] = useState([]);
+  // 추가 - 확인 함수
+  const confirmAddEducation = (targetEducation) => {
+    // TODO : 학교이름, 전공 유효성 검사
+    targetEducation.id = Date.now();
+    const resultEducations = [...educations, targetEducation];
+    setEducations([...resultEducations]);
+    setIsAdding(false);
+  };
 
-    // 추가 - 확인 함수
-    const confirmAddEducation = (targetEducation) => {
-        // TODO : 학교이름, 전공 유효성 검사
-        targetEducation.id = Date.now();
-        const resultEducations = [...educations, targetEducation];
-        setEducations([...resultEducations]);
-        setIsAdding(false);
-    };
+  const cancelAddEducation = () => {
+    setIsAdding(false);
+  };
 
-    const cancelAddEducation = () => {
-        setIsAdding(false);
-    };
+  return (
+    <Card className="mb-2 ms-3 mr-5 ">
+      <Card.Body>
+        <Card.Title>학력</Card.Title>
+        <EducationCard educations={educations} setEducations={setEducations} />
+      </Card.Body>
 
-    return (
-        <Card className="mb-2 ms-3 mr-5 ">
-            <Card.Body>
-                <Card.Title>학력</Card.Title>
-                <EducationCard educations={educations} setEducations={setEducations}/>
-            </Card.Body>
-
-            <div style={{display: "flex", justifyContent: "center"}}>
-                {isEditable ? (
-                    <button className="btn btn-primary" onClick={toggleAddEducationForm}>
-                        +
-                    </button>
-                ) : null}
-            </div>
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "flex-end",
-                    margin: "0 0 1rem 0",
-                }}
-            >
-                {isAdding ? (
-                    <EducationForm
-                        onConfirm={confirmAddEducation}
-                        onCancel={cancelAddEducation}
-                        education={{
-                            id: null,
-                            schoolName: "",
-                            major: "",
-                            position: "재학중",
-                        }}
-                    />
-                ) : null}
-            </div>
-        </Card>
-    );
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {isEditable ? (
+          <button className="btn btn-primary" onClick={toggleAddEducationForm}>
+            +
+          </button>
+        ) : null}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          margin: "0 0 1rem 0",
+        }}
+      >
+        {isAdding ? (
+          <EducationForm
+            onConfirm={confirmAddEducation}
+            onCancel={cancelAddEducation}
+            education={{
+              id: null,
+              schoolName: "",
+              major: "",
+              position: "재학중",
+            }}
+          />
+        ) : null}
+      </div>
+    </Card>
+  );
 };
 
 export default Education;
--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -1,85 +1,95 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../../../src/index.css";
 import EducationForm from "./EducationForm";
 
-function EducationCard({
-                           educations,
-                           setEducations,
-                       }) {
+function EducationCard({ educations, setEducations }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [targetId, setTargetId] = useState(null);
+  const toggleEditEducationForm = (id) => {
+    if (targetId === id && isEditing) {
+      setTargetId(null);
+      setIsEditing(false);
+    } else {
+      setTargetId(id);
+      setIsEditing(true);
+    }
+  };
 
-    const [isEditing, setIsEditing] = useState(false);
-    const [targetId, setTargetId] = useState(null);
-    const toggleEditEducationForm = (id) => {
-        if (targetId === id && isEditing) {
-            setTargetId(null);
-            setIsEditing(false);
-        } else {
-            setTargetId(id);
-            setIsEditing(true);
-        }
+  // 편집 - 확인 함수
+  const confirmEditEducation = (targetEducation) => {
+    // TODO : 학교이름, 전공 유효성 검사
+    const resultEducations = [...educations];
+    resultEducations[
+      resultEducations.findIndex(
+        (education) => education.id === targetEducation.id
+      )
+    ] = {
+      ...targetEducation,
     };
+    setEducations([...resultEducations]);
+    cancelEditEducation();
+  };
 
-    // 편집 - 확인 함수
-    const confirmEditEducation = (targetEducation) => {
-        // TODO : 학교이름, 전공 유효성 검사
-        const resultEducations = [...educations];
-        resultEducations[resultEducations.findIndex(education => education.id === targetEducation.id)] = {
-            ...targetEducation
-        }
-        setEducations([...resultEducations]);
-        cancelEditEducation();
-    };
+  const cancelEditEducation = () => {
+    setIsEditing(false);
+    setTargetId(null);
+  };
 
-    const cancelEditEducation = () => {
-        setIsEditing(false);
-        setTargetId(null);
-    };
+  const onRemove = (id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    // console.log("remove: ", educations, education.id, targetEducation);
+    setEducations(educations.filter((education) => education.id !== id));
+  };
 
-    return (
-        <div>
-            <div>
-                {educations.map((education, index) => {
-                    return (
-                        <p key={education.id}>
-                            <div className="align-items-center row margin_tb10">
-                                <div className="col">
-                                    <div>{education.schoolName}</div>
-                                    <div>
-                                        {education.major}({education.position})
-                                    </div>
-                                </div>
-                                <div
-                                    className="col-lg-1 col"
-                                    style={{width: "150px", height: "35px", display: "flex"}}
-                                >
-                                    <button
-                                        type="button"
-                                        className="mr-3 btn btn-outline-info btn-sm"
-                                        onClick={() => toggleEditEducationForm(education.id)}
-                                    >
-                                        편집
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="mr-3 btn btn-outline-info btn-sm"
-                                    >
-                                        삭제
-                                    </button>
-                                </div>
-                            </div>
-                            {(isEditing && education.id === targetId) &&
-                                <EducationForm education={{
-                                    ...education
-                                }}
-                                               onConfirm={confirmEditEducation}
-                                               onCancel={cancelEditEducation}
-                                />}
-                        </p>
-                    );
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        {educations.map((education, index) => {
+          return (
+            <p key={education.id}>
+              <div className="align-items-center row margin_tb10">
+                <div className="col">
+                  <div>{education.schoolName}</div>
+                  <div>
+                    {education.major}({education.position})
+                  </div>
+                </div>
+                <div
+                  className="col-lg-1 col"
+                  style={{ width: "150px", height: "35px", display: "flex" }}
+                >
+                  <button
+                    type="button"
+                    className="mr-3 btn btn-outline-info btn-sm"
+                    onClick={() => toggleEditEducationForm(education.id)}
+                  >
+                    편집
+                  </button>
+                  <button
+                    type="button"
+                    className="mr-3 btn btn-outline-info btn-sm"
+                    onClick={() => onRemove(education.id)}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </div>
+              {isEditing && education.id === targetId && (
+                <EducationForm
+                  education={{
+                    ...education,
+                  }}
+                  onConfirm={confirmEditEducation}
+                  onCancel={cancelEditEducation}
+                />
+              )}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default EducationCard;
