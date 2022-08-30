859ca218d2158ea74f9b4df56711276e53d30dd7
Seongwoo Shin <asdfgg77@naver.com>
2022-08-30 23:03:29 +0900

    if...else문을 switch문으로 수정

--- back/src/services/searchService.js
+++ back/src/services/searchService.js
@@ -1,21 +1,27 @@
-import { User } from "../db/models/User";
 import { ERRORS } from "../constants/constants";
 
 const searchService = {
   getSearches: async ({ option, contents }) => {
     let searchOption;
-    if (option === "name") {
-      searchOption = { name: new RegExp(contents) };
-    } else if (option === "email") {
-      searchOption = { email: new RegExp(contents) };
-    } else if (option === "description") {
-      searchOption = { description: new RegExp(contents) };
-    } else if (option === "all") {
-      searchOption = { contents: new RegExp(contents) };
-    } else {
-      throw new Error(ERRORS.BODY_DATA_ERROR.errorCode);
+
+    switch (option) {
+      case "name":
+        searchOption = { name: new RegExp(contents) };
+        break;
+      case "email":
+        searchOption = { email: new RegExp(contents) };
+        break;
+      case "description":
+        searchOption = { description: new RegExp(contents) };
+        break;
+      case "all":
+        searchOption = { contents: new RegExp(contents) };
+        break;
+
+      default:
+        throw new Error(ERRORS.BODY_DATA_ERROR.errorCode);
     }
-    const searches = await User.findSearchData(searchOption);
+
     return searches;
   },
 };
