7a7dd0280115fabb67f47e6990bb84c52bec00f1
이 안토니의호 <ehlee3275@gmail.com>
2022-08-30 11:46:57 +0900

    파일 삭제 및 정리

--- front/src/components/SideBar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Col } from "react-bootstrap";
-import "../index.css";
-function SideBar() {
-  return <Col md="2" lg="1"></Col>;
-}
-
-export default SideBar;
--- front/src/darkmode/name.js
+++
@@ -1,5 +0,0 @@
-const name = (watchName) => {
-  return <div>fff{watchName}</div>;
-};
-
-export default name;
