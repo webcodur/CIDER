d9d567070725119344e5e6281eef2813aa8afa75
이 안토니의호 <ehlee3275@gmail.com>
2022-08-30 14:16:32 +0900

    전체적인 정리 완료

--- front/src/components/user/LoginForm.js
+++ front/src/components/user/LoginForm.js
@@ -5,16 +5,13 @@ import { Container, Col, Row, Form, Button } from "react-bootstrap";
 import * as Api from "../../api";
 import { DispatchContext } from "../../App";
 
-function LoginForm({isEditable}) {
+function LoginForm({ isEditable }) {
   const navigate = useNavigate();
   const dispatch = useContext(DispatchContext);
 
-  //useState로 email 상태를 생성함.
   const [email, setEmail] = useState("");
-  //useState로 password 상태를 생성함.
   const [password, setPassword] = useState("");
 
-  //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
   const validateEmail = (email) => {
     return email
       .toLowerCase()
@@ -23,38 +20,27 @@ function LoginForm({isEditable}) {
       );
   };
 
-  //위 validateEmail 함수를 통해 이메일 형태 적합 여부를 확인함.
   const isEmailValid = validateEmail(email);
-  // 비밀번호가 4글자 이상인지 여부를 확인함.
   const isPasswordValid = password.length >= 4;
-  //
-  // 이메일과 비밀번호 조건이 동시에 만족되는지 확인함.
   const isFormValid = isEmailValid && isPasswordValid;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // "user/login" 엔드포인트로 post요청함.
       const res = await Api.post("user/login", {
         email,
         password,
       });
-      // 유저 정보는 response의 data임.
       const user = res.data;
-      // JWT 토큰은 유저 정보의 token임.
       const jwtToken = user.token;
-      // sessionStorage에 "userToken"이라는 키로 JWT 토큰을 저장함.
       sessionStorage.setItem("userToken", jwtToken);
-      // dispatch 함수를 이용해 로그인 성공 상태로 만듦.
       dispatch({
         type: "LOGIN_SUCCESS",
         payload: user,
       });
 
-      // 기본 페이지로 이동함.
-      // navigate("/", { replace: true }); // 원래 코드 
-      navigate("/", { state:{email} });
+      navigate("/", { state: { email } });
     } catch (err) {
       console.log("로그인에 실패하였습니다.\n", err);
     }
@@ -65,8 +51,6 @@ function LoginForm({isEditable}) {
       <Row className="justify-content-md-center mt-5">
         <Col lg={8}>
           <Form onSubmit={handleSubmit}>
-
-            
             <Form.Group controlId="loginEmail">
               <Form.Label>이메일 주소</Form.Label>
               <Form.Control
@@ -81,7 +65,6 @@ function LoginForm({isEditable}) {
                 </Form.Text>
               )}
             </Form.Group>
-
 
             <Form.Group controlId="loginPassword" className="mt-3">
               <Form.Label>비밀번호</Form.Label>
@@ -98,7 +81,6 @@ function LoginForm({isEditable}) {
               )}
             </Form.Group>
 
-
             <Form.Group as={Row} className="mt-3 text-center">
               <Col sm={{ span: 20 }}>
                 <Button variant="primary" type="submit" disabled={!isFormValid}>
@@ -107,7 +89,6 @@ function LoginForm({isEditable}) {
               </Col>
             </Form.Group>
 
-
             <Form.Group as={Row} className="mt-3 text-center">
               <Col sm={{ span: 20 }}>
                 <Button variant="light" onClick={() => navigate("/register")}>
@@ -115,8 +96,6 @@ function LoginForm({isEditable}) {
                 </Button>
               </Col>
             </Form.Group>
-
-
           </Form>
         </Col>
       </Row>
--- front/src/components/user/RegisterForm.js
+++ front/src/components/user/RegisterForm.js
@@ -6,16 +6,11 @@ import * as Api from "../../api";
 function RegisterForm() {
   const navigate = useNavigate();
 
-  //useState로 email 상태를 생성함.
   const [email, setEmail] = useState("");
-  //useState로 password 상태를 생성함.
   const [password, setPassword] = useState("");
-  //useState로 confirmPassword 상태를 생성함.
   const [confirmPassword, setConfirmPassword] = useState("");
-  //useState로 name 상태를 생성함.
   const [name, setName] = useState("");
 
-  //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
   const validateEmail = (email) => {
     return email
       .toLowerCase()
@@ -24,16 +19,11 @@ function RegisterForm() {
       );
   };
 
-  //위 validateEmail 함수를 통해 이메일 형태 적합 여부를 확인함.
   const isEmailValid = validateEmail(email);
-  // 비밀번호가 4글자 이상인지 여부를 확인함.
   const isPasswordValid = password.length >= 4;
-  // 비밀번호와 확인용 비밀번호가 일치하는지 여부를 확인함.
   const isPasswordSame = password === confirmPassword;
-  // 이름이 2글자 이상인지 여부를 확인함.
   const isNameValid = name.length >= 2;
 
-  // 위 4개 조건이 모두 동시에 만족되는지 여부를 확인함.
   const isFormValid =
     isEmailValid && isPasswordValid && isPasswordSame && isNameValid;
 
@@ -41,14 +31,12 @@ function RegisterForm() {
     e.preventDefault();
 
     try {
-      // "user/register" 엔드포인트로 post요청함.
       await Api.post("user/register", {
         email,
         password,
         name,
       });
 
-      // 로그인 페이지로 이동함.
       navigate("/login");
     } catch (err) {
       console.log("회원가입에 실패하였습니다.", err);
