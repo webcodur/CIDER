6a186971f3500db42917922097ee6ef49bebf8c7
Hanuel Lee <wsxedc901@gmail.com>
2022-08-31 17:01:22 +0900

    좋아요 기능 구현

--- front/src/components/UI/LikeButton.js
+++ front/src/components/UI/LikeButton.js
@@ -1,32 +1,90 @@
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import * as Api from '../../api';
-import { Button } from 'react-bootstrap';
-import { useState, useContext } from 'react';
+
+import { Button, Overlay, Tooltip } from 'react-bootstrap';
 import AuthContext from '../project/stores/AuthContext';
+import { UserStateContext } from '../../App';
 
 const LikeButton = (props) => {
   const [isClicked, setIsClicked] = useState(false);
+  const [isShowingTooltip, setIsShowingTooltip] = useState(false);
+  const [count, setCount] = useState(0);
+  const [tooltipText, setToolTipText] = useState('');
+
+  let tempCount = props.user?.likes.length ? props.user.likes.length : null;
+
   const context = useContext(AuthContext);
+  const userState = useContext(UserStateContext);
+
+  const target = useRef(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShowingTooltip(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [isShowingTooltip]);
 
   const updateLikes = async () => {
-    setIsClicked(!isClicked);
-
-    let body = { direction: 'false' };
-
-    if (isClicked) {
-      body = { direction: 'true' };
+    if (!count) {
+      setCount(tempCount);
     }
 
+    if (props.user.likes.includes(userState.user.id)) {
+      setCount(tempCount - 1);
+      setToolTipText('-');
+    }
+
+    if (props.user.likes.includes(userState.user.id) && isClicked) {
+      setCount(tempCount);
+      setToolTipText('+');
+    }
+
+    if (!props.user.likes.includes(userState.user.id)) {
+      setCount(tempCount + 1);
+      setToolTipText('+');
+    }
+
+    if (!props.user.likes.includes(userState.user.id) && isClicked) {
+      setCount(tempCount);
+      setToolTipText('-');
+    }
+
+    setIsClicked(!isClicked);
+    console.log(isClicked);
+    setIsShowingTooltip(true);
+
     try {
-      await Api.patch(props.user.id, 'likes', body);
+      await Api.patch(props.user.id, 'likes');
     } catch (err) {
       context.setModalText('좋아요 요청을 실패했습니다.');
     }
   };
 
   return (
-    <Button variant="outline-info" size="sm" onClick={updateLikes}>
-      👍
-    </Button>
+    <React.Fragment>
+      <Button
+        ref={target}
+        className={`${context.isDarkMode ? 'dark' : ''}`}
+        variant="outline-info"
+        size="sm"
+        onClick={updateLikes}
+      >
+        {count || (!isClicked && tempCount)} 👍
+      </Button>
+      <Overlay
+        target={target.current}
+        show={isShowingTooltip}
+        placement="right"
+      >
+        {(props) => (
+          <Tooltip className="lightblue-tooltip" {...props}>
+            {tooltipText}1!
+          </Tooltip>
+        )}
+      </Overlay>
+    </React.Fragment>
   );
 };
 
