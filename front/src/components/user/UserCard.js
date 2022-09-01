a0413162db2c69279bc576ac3b97b8aef1af6f85
이 안토니의호 <ehlee3275@gmail.com>
2022-09-01 15:07:00 +0900

    에러 수정

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -1,12 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import { Card, Row, Button, Col } from 'react-bootstrap';
-import React, { useState, useContext, useEffect } from 'react';
-import { UserStateContext } from '../../App';
-import LikeButton from '../UI/LikeButton';
-import * as Api from '../../api';
-import { useTheme } from '../darkmode/themeProvider';
-import '../../../src/styles/index.css';
-import DisplayToggleComp from '../DisplayToggleComp';
+import { useNavigate } from "react-router-dom";
+import { Card, Row, Button, Col } from "react-bootstrap";
+import React, { useState, useContext, useEffect } from "react";
+import { UserStateContext } from "../../App";
+import LikeButton from "../UI/LikeButton";
+import * as Api from "../../api";
+import { useTheme } from "../darkmode/themeProvider";
+import "../../../src/styles/index.css";
+import DisplayToggleComp from "../DisplayToggleComp";
 
 function UserCard({
   user,
@@ -22,19 +22,19 @@ function UserCard({
   const ThemeMode = useTheme();
   const theme = ThemeMode[0];
   const id = userState?.user?.id;
-  let userstr = '';
+  let userstr = "";
 
   useEffect(() => {
     Api.get(`${user?.id ? user?.id : id}/images/profile`).then((res) => {
       setPhoto(res.data);
       console.log();
 
-      console.log('res', res.data);
+      console.log("res", res.data);
     });
   }, [user?.id ? user?.id : id]);
 
   function recentlyView() {
-    let origin = localStorage.getItem('recentlyView1');
+    let origin = localStorage.getItem("recentlyView1");
     if (!origin) {
       userstr = JSON.stringify([{ name: user?.name, id: user?.id }]);
     } else {
@@ -45,27 +45,27 @@ function UserCard({
       userstr = JSON.stringify([...origin, { name: user?.name, id: user?.id }]);
     }
     navigate(`/users/${user.id}`);
-    localStorage.setItem('recentlyView1', userstr);
+    localStorage.setItem("recentlyView1", userstr);
   }
 
   const str = user?.id ? user.id : id;
   const regex = /[^0-9]/g;
-  let result = '';
+  let result = "";
   if (str) {
-    result = str.replace(regex, '');
+    result = str.replace(regex, "");
   }
   const slicenum = result.slice(0, 3);
   const number = parseInt(slicenum);
   return (
     <Card
       className="mb-2 ms-3 mr-5"
-      style={{ width: '18rem' }}
-      id={theme == 'light' ? 'light' : 'dark'}
+      style={{ width: "18rem" }}
+      id={theme == "light" ? "light" : "dark"}
     >
       <Card.Body>
         <Row className="justify-content-md-center">
           <Card.Img
-            style={{ width: '10rem', height: '8rem' }}
+            style={{ width: "10rem", height: "8rem" }}
             className="mb-3"
             src={photo}
             alt="사용자 프로필 사진"
@@ -112,5 +112,3 @@ function UserCard({
 }
 
 export default UserCard;
-<<<<<<< HEAD
-=======
