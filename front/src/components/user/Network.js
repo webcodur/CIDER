141f4bd5140aec25ecd31a0f09e4b3d6ad519eba
이 안토니의호 <ehlee3275@gmail.com>
2022-09-01 16:38:43 +0900

    다크모드랑 검색 수정

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserStateContext } from '../../App';
-import { Container, Row, Col } from 'react-bootstrap';
-import '../../styles/index.css';
-import * as Api from '../../api';
-import UserCard from './UserCard';
-import SearchBar from './SearchBar';
-import Anchor from '../UI/Anchor';
+import React, { useEffect, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserStateContext } from "../../App";
+import { Container, Row, Col } from "react-bootstrap";
+import "../../styles/index.css";
+import { useTheme } from "../../components/darkmode/themeProvider";
+import * as Api from "../../api";
+import UserCard from "./UserCard";
+import SearchBar from "./SearchBar";
+import Anchor from "../UI/Anchor";
 
 function Network() {
   const navigate = useNavigate();
@@ -14,13 +15,15 @@ function Network() {
   const [users, setUsers] = useState([]);
   const [searchData, setSearchData] = useState([]);
   const [isEmpty, setIsEmpty] = useState(false);
+  const ThemeMode = useTheme();
+  const theme = ThemeMode[0];
 
   useEffect(() => {
     if (!userState.user) {
-      navigate('/login');
+      navigate("/login");
       return;
     }
-    Api.get('userlist').then((res) => setUsers(res.data));
+    Api.get("userlist").then((res) => setUsers(res.data));
   }, [userState, navigate]);
 
   return (
@@ -42,8 +45,9 @@ function Network() {
               <div
                 className="nodata"
                 style={{
-                  width: '1300px',
+                  width: "1300px",
                 }}
+                id={theme == "light" ? "blight" : "bdark"}
               >
                 검색 결과가 없습니다
               </div>
@@ -58,7 +62,7 @@ function Network() {
         </Col>
         <Anchor />
       </Container>
-      <div style={{ width: '15%' }}></div>
+      <div style={{ width: "15%" }}></div>
     </div>
   );
 }
