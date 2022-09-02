849a7d932706a4a37d6d7072eb45638f5b5d58ef
이 안토니의호 <ehlee3275@gmail.com>
2022-09-02 22:11:58 +0900

    검색시 get 요청 무제한 보내는거 막음

--- front/src/components/user/SearchBar.js
+++ front/src/components/user/SearchBar.js
@@ -1,12 +1,26 @@
 import * as Api from "../../api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import searchBar from "../../styles/searchBar.css";
 import { Form } from "react-bootstrap";
 const Search = ({ setSearchData, setIsEmpty }) => {
   const [Selected, setSelected] = useState("all");
   const [inputValue, setInputValue] = useState("");
+  var getSearch = "";
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      test();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [inputValue]);
+
   const test = () => {
-    setTimeout(() => {
+    if (getSearch) {
+      clearTimeout(getSearch);
+    }
+
+    getSearch = setTimeout(() => {
       Api.get2(`search?option=${Selected}&contents=${inputValue}`).then(
         (res) => {
           setSearchData(res.data);
@@ -27,15 +41,7 @@ const Search = ({ setSearchData, setIsEmpty }) => {
 
   return (
     <div css={{ searchBar }} id="formInput">
-      {/* <select onChange={handleChangeSelect}>
-        <option value="all">통합 검색</option>
-        <option value="name">이름</option>
-        <option value="email">이메일</option>
-        <option value="description">내용</option>
-      </select> */}
-
       <form
-        // id="formInput"
         id="formwidth"
         name="profile"
         onChange={handleChangeInput}
@@ -56,20 +62,13 @@ const Search = ({ setSearchData, setIsEmpty }) => {
           <Form.Control
             type="search"
             className="form-control rounded formsearch"
-            // placeholder="Search"
             aria-label="Search"
             aria-describedby="search-addon"
-            onKeyUpCapture={test}
-            // style={{ height: "38px", width: "150px" }}
             style={{ Width: "100%", minWidth: "150px" }}
           />
           <input style={{ display: "none " }} />
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
-
-        {/* <button type="button" class="btn btn-outline-primary" onClick={test}>
-            search
-          </button> */}
       </form>
     </div>
   );
